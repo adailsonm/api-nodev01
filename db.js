404b5c324f3b54816b18862c421cab0f5bc8f5c6
Adailson Moreira <adailson.moreira16@gmail.com>
2018-03-09 03:06:47 -0300

    First Commit

---
+++ db.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/apitest');
+
+var customersSchema = new mongoose.Schema({
+    name: String,
+    email: String
+}, { collection: 'customers'}
+);
+
+
+mongoose.model('customers', customersSchema);
+
+module.exports = { Mongoose: mongoose, customerSchema: customersSchema }
